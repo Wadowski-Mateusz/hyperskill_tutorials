bf2fc4a6528541743e5322e7932c474ef548b7f1
Wadowski-Mateusz <mateusz.wadow@gmail.com>
2023-04-07 20:04:17 +0200

    Stage 3/6

--- Encryption-Decryption (Kotlin)/Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -1,20 +1,27 @@
 package encryptdecrypt
 
 fun main() {
-    val msg = readln().lowercase()
-    val key = readln().toInt()
-//    val msg = "welcode to hyperskill"
-//    val key = 5
-
-    val encMsg = buildString {
-        for (char in msg) {
-            if (char in 'a'..'z')
-                append( ((char.code - 'a'.code + key) % 26 + 'a'.code).toChar()  )
-            else
-                append(char)
-        }
+    val choice: String = readln()
+    val message: String = readln()
+    val key: Int = readln().toInt()
+    val output: String = when (choice) {
+        "enc" -> encryption(message, key)
+        "dec" -> decryption(message, key)
+        else -> "No such a command!"
     }
-
-    println(encMsg)
-
+    println(output)
 }
+
+fun encryption(message: String, key: Int): String =
+    buildString { message.map { append((it.code + key).toChar()) } }
+//    val encMsg = buildString {
+//        for (char in msg) {
+//            if (char in 'a'..'z')
+//                append( ((char.code - 'a'.code + key) % 26 + 'a'.code).toChar()  )
+//            else
+//                append((char.code).toChar())
+//        }
+//    }
+
+fun decryption(ciphertext: String, key: Int): String =
+    buildString { ciphertext.map { append((it.code - key).toChar()) } }
