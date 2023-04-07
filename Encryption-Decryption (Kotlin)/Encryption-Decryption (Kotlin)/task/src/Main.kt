1dae6ea4291717b510b0e5b8b1b354a619393cac
Wadowski-Mateusz <mateusz.wadow@gmail.com>
2023-04-07 22:24:34 +0200

    Stage 5/6

--- Encryption-Decryption (Kotlin)/Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -1,9 +1,13 @@
 package encryptdecrypt
 
+import java.io.File
+
 fun main(args: Array<String>) {
     var mode: String = "dec"
     var data: String = ""
     var key: Int = 0
+    var inputFilename: String = ""
+    var outputFilename: String = ""
 
     if (args.isNotEmpty()) {
         if (args.indexOf("-mode") != -1)
@@ -12,14 +16,42 @@ fun main(args: Array<String>) {
             key = args[args.indexOf("-key") + 1].toInt()
         if (args.indexOf("-data") != -1)
             data = args[args.indexOf("-data") + 1]
+        if (args.indexOf("-in") != -1)
+            inputFilename = args[args.indexOf("-in") + 1]
+        if (args.indexOf("-out") != -1)
+            outputFilename = args[args.indexOf("-out") + 1]
     }
 
-    val output: String = when (mode) {
-        "enc" -> encryption(data, key)
-        "dec" -> decryption(data, key)
-        else -> "No such a command!"
+    val inputString: String =
+        try {
+            if (data.isNotEmpty())
+                data
+            else if (inputFilename.isNotEmpty())
+                readFromFile(inputFilename)
+            else
+                ""
+        } catch (e: Exception) {
+            println("Error")
+            return
+        }
+
+    val message: String =
+        when (mode) {
+            "enc" -> encryption(inputString, key)
+            "dec" -> decryption(inputString, key)
+            else -> {println("No such a command!"); return}
+        }
+
+    try {
+        if (outputFilename.isEmpty())
+            println(message)
+        else
+            writeToFile(outputFilename, message)
+    } catch (e: Exception) {
+        println("Error")
+        return
     }
-    println(output)
+
 }
 
 fun encryption(message: String, key: Int): String =
@@ -27,6 +59,11 @@ fun encryption(message: String, key: Int): String =
 
 fun decryption(ciphertext: String, key: Int): String =
     buildString { ciphertext.map { append((it.code - key).toChar()) } }
+
+fun readFromFile(inputFilename: String): String = File(inputFilename).readText()
+
+fun writeToFile(outputFilename: String, message: String) = File(outputFilename).writeText(message)
+
 
 //  // encryption
 //    val encMsg = buildString {
