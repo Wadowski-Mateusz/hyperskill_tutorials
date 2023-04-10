da404dd9c5993c356ed0dad36e884814fb60d547
Wadowski-Mateusz <mateusz.wadow@gmail.com>
2023-04-10 21:47:17 +0200

    Getting data from REST

---
+++ Introduction to Spring Web MVC/src/main/kotlin/mateusz/introduction/demo/Controlers/TaskController.kt
@@ -0,0 +1,31 @@
+package mateusz.introduction.demo.Controlers
+
+import mateusz.introduction.demo.Task
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TaskController {
+    @GetMapping("/test")
+    fun returnOne(): Int {
+        return 1
+    }
+
+    val taskList = listOf(
+        Task(1, "task1", "A first test task", false),
+        Task(2, "task2", "A second test task", true)
+    )
+
+    @GetMapping("/tasks")
+    fun getTasks(): List<Task> {
+        return taskList
+    }
+
+    @GetMapping("/tasks/{id}")
+    fun getTasks(@PathVariable id: Int): ResponseEntity<Task?>? {
+        return ResponseEntity(taskList[id - 1], HttpStatus.ACCEPTED)
+    }
+}
---
+++ Introduction to Spring Web MVC/src/main/kotlin/mateusz/introduction/demo/Task.kt
@@ -0,0 +1,10 @@
+package mateusz.introduction.demo
+
+class Task(
+    var id: Int,
+    var name: String,
+    var description: String,
+    var completed: Boolean
+) {
+
+}
