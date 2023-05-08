41bddf8d9c018839462a9fe3cd9617b1c4d2fc0c
Wadowski-Mateusz <mateusz.wadow@gmail.com>
2023-05-08 13:02:26 +0200

    Stage 4/5

--- Android/Secret Diary/Secret Diary/task/src/main/java/org/hyperskill/secretdiary/MainActivity.kt
+++ Android/Secret Diary/Secret Diary/task/src/main/java/org/hyperskill/secretdiary/MainActivity.kt
@@ -1,7 +1,10 @@
 package org.hyperskill.secretdiary
 
 import android.app.AlertDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -12,14 +15,22 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
+const val PREF_DIARY = "PREF_DIARY"
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val textView = findViewById<TextView>(R.id.tvDiary)
+        textView.movementMethod = ScrollingMovementMethod() // scrolling vertically
         val input = findViewById<EditText>(R.id.etNewWriting)
+
+        sharedPreferences = getSharedPreferences(PREF_DIARY, Context.MODE_PRIVATE)
+        loadNotes(textView)
 
         val save = findViewById<Button>(R.id.btnSave)
         save.setOnClickListener {
@@ -30,6 +41,7 @@ class MainActivity : AppCompatActivity() {
                     if(textView.text.isNotEmpty()) "${timeStamp}\n${input.text}\n\n${textView.text}"
                     else "${timeStamp}\n${input.text}"
                 input.text.clear()
+                saveNotes(textView)
             } else Toast.makeText(
                 applicationContext,
                 "Empty or blank input cannot be saved",
@@ -41,7 +53,10 @@ class MainActivity : AppCompatActivity() {
         val dialog = AlertDialog.Builder(this)
             .setTitle("Remove last note")
             .setMessage("Do you really want to remove the last writing? This operation Cannot be undone!")
-            .setPositiveButton("Yes") { _, _ -> removeLastNote(textView) }
+            .setPositiveButton("Yes") { _, _ ->
+                removeLastNote(textView)
+                saveNotes(textView)
+            }
             .setNegativeButton("No", null)
 
         undo.setOnClickListener {
@@ -58,5 +73,15 @@ class MainActivity : AppCompatActivity() {
                 else ""
         }
     }
+
+    private fun saveNotes(textView: TextView) {
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString("KEY_DIARY_TEXT", textView.text.toString()).apply()
+    }
+
+    private fun loadNotes(textView: TextView) {
+        textView.text = sharedPreferences.getString("KEY_DIARY_TEXT", "") // like Map.getOrDefault()
+    }
+
 
 }
